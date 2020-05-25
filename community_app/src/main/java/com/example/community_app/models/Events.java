7dc81c6eb81edf732229c92de6b9848aca57c6cf
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 05:14:38 -0500

    fixed error; now adding columns w/in model file of events

--- community_app/src/main/java/com/example/community_app/models/Events.java
+++ community_app/src/main/java/com/example/community_app/models/Events.java
@@ -1,8 +1,26 @@
 package com.example.community_app.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 //this annotation specifies that the class is an entity and is mapped to a database table.
 @Entity
+@Table(name = "event_info")
 public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int eventid;
+
+    @Column
+    private String event_name;
+
+    @Column
+    private String event_location;
+
+    @Column
+    private String event_date;
+
+    @Column
+    private String event_summary;
+
 }
