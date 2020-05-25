8eaf94dc1d9d4b98b8c86cde1e63b7846f9f6e38
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 04:28:25 -0500

    changed method to make more sense regarding me getting info from database

--- community_app/src/main/java/com/example/community_app/controller/Event.java
+++ community_app/src/main/java/com/example/community_app/controller/Event.java
@@ -1,13 +1,16 @@
 package com.example.community_app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //annotation
 @RestController
+//this annotation allows me to no longer need to specify endpoint in @GetMapping
+@RequestMapping("/events")
 public class Event {
-    @GetMapping("/events")
-    public String heyEvents(){
+    @GetMapping
+    public String getEvents() {
         return "Hey! this is your event endpoint.";
     }
 
