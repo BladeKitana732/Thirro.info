9e6c3e2d30ec8da9181f40f41c2b900a4f2972bb
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 03:48:23 -0500

    testing events endpoint in backend :D she connected guys!

--- community_app/src/main/java/com/example/community_app/controller/Event.java
+++ community_app/src/main/java/com/example/community_app/controller/Event.java
@@ -1,7 +1,14 @@
 package com.example.community_app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+//annotation
+@RestController
 public class Event {
+    @GetMapping("/events")
+    public String heyEvents(){
+        return "Hey! this is your event endpoint.";
+    }
 
 }
