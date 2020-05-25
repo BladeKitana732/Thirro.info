7c5472925bfc19aaf0bc4e587959ff2ed8d60284
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 05:36:33 -0500

    helpful resource commented that helped me understand purpose

--- community_app/src/main/java/com/example/community_app/repository/EventRepo.java
+++ community_app/src/main/java/com/example/community_app/repository/EventRepo.java
@@ -3,7 +3,7 @@ package com.example.community_app.repository;
 //repository is a mechanism for encapsulating storage, retrieval, and search behavior which emulates a collection of objects; this is an interface
 
 import com.example.community_app.models.Events;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository; //https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
 import org.springframework.stereotype.Repository;
 
 @Repository
