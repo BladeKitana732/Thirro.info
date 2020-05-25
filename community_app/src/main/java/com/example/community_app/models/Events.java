7d3b3e944b9160b1a79ce5c66c3407b463bf581f
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 04:53:07 -0500

    had to update pom.xml to ensure annotation of entity would work

--- community_app/src/main/java/com/example/community_app/models/Events.java
+++ community_app/src/main/java/com/example/community_app/models/Events.java
@@ -1,6 +1,8 @@
 package com.example.community_app.models;
 
+import javax.persistence.Entity;
 
-
+//this annotation specifies that the class is an entity and is mapped to a database table.
+@Entity
 public class Events {
 }
