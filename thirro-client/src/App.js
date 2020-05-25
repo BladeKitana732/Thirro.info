9d57689fe1eb2bd15830db4342ff6a0f6379f725
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 06:55:28 -0500

    my very own api rendering in console. *tears of happiness*

--- thirro-client/src/App.js
+++ thirro-client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  async function backendInfoTest() {
+    const response = await axios.get(`http://localhost:8080/events`);
+    console.log(response.data);
+
+  }
   return (
     <div className="App">
-  
+    <button onClick={backendInfoTest}>Click me to render api you just made! :D</button>
     </div>
   );
 }
