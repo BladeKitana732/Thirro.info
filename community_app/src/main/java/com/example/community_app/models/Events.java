7fa81e02d0dd3e86ec10e899abbf52419da881dc
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 05:21:03 -0500

    commented explanation for usage of annotations; getters and setters placed

--- community_app/src/main/java/com/example/community_app/models/Events.java
+++ community_app/src/main/java/com/example/community_app/models/Events.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 @Entity
 @Table(name = "event_info")
 public class Events {
+
+    //explanation of annotations used below: https://stackoverflow.com/questions/20603638/what-is-the-use-of-annotations-id-and-generatedvaluestrategy-generationtype
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
@@ -23,4 +25,51 @@ public class Events {
     @Column
     private String event_summary;
 
+    //constructor for object
+    public Events(){ }
+
+    //getters and setters
+
+
+    public void setEventid(int eventid) {
+        this.eventid = eventid;
+    }
+
+    public int getEventid() {
+        return eventid;
+    }
+
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public void setEvent_name(String event_name) {
+        this.event_name = event_name;
+    }
+
+    public String getEvent_date() {
+        return event_date;
+    }
+
+    public void setEvent_date(String event_date) {
+        this.event_date = event_date;
+    }
+
+    public String getEvent_location() {
+        return event_location;
+    }
+
+    public void setEvent_location(String event_location) {
+        this.event_location = event_location;
+    }
+
+    public String getEvent_summary() {
+        return event_summary;
+    }
+
+    public void setEvent_summary(String event_summary) {
+        this.event_summary = event_summary;
+    }
+
 }
+
