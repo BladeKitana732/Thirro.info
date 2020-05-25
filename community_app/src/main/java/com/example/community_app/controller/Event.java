1d6bc9d206758c04e9f0b3ceb8f97923cd876f9d
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 05:44:05 -0500

    imported models file; now need service file made which is also interface

--- community_app/src/main/java/com/example/community_app/controller/Event.java
+++ community_app/src/main/java/com/example/community_app/controller/Event.java
@@ -1,5 +1,6 @@
 package com.example.community_app.controller;
 
+import com.example.community_app.models.Events;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 //this annotation allows me to no longer need to specify endpoint in @GetMapping
 @RequestMapping("/events")
 public class Event {
+
+//    @GetMapping
+//    public String getEvents() {
+//        return "Hey! this is your event endpoint.";
+//    }
+
     @GetMapping
-    public String getEvents() {
-        return "Hey! this is your event endpoint.";
+    public Iterable<Events> getEvents() {
+        return eventService.getEvents();
     }
+
+
+
+
+
+
+
 
 }
 
