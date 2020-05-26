99f952e5e26e251bad22758d71f374509f747ffc
Teresa Malave <dev.malave7@gmail.com>
2020-05-26 03:24:17 -0500

    same method once more for review endpoint

--- thirro-client/src/App.js
+++ thirro-client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 // import Events from './components/Events';
-import DevLeaders from './components/DevLeaders';
+// import DevLeaders from './components/DevLeaders';
+import Reviews from './components/Reviews';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
 
-    <DevLeaders />
+    <Reviews />
    
     </div>
   );
---
+++ thirro-client/src/components/Reviews.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Reviews extends Component {
+    
+    state = {
+        review: []
+    }
+
+    componentDidMount() {
+        let reviewsApi = `https://thirro-info.herokuapp.com/reviews`;
+        
+        axios.get(reviewsApi)
+    
+        .then((Response) => {
+            const reviews= Response.data.map(details => details.message);
+    
+            console.log(reviews);
+    
+            this.setState({
+                reviewMessage: reviews
+            })
+        })
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Thirro.DevLeaders!</h1>
+                <h2>{this.state.reviewMessage}</h2>
+            </div>
+        )
+    }
+}
