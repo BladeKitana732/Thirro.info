ab1fcfe3eab2a486c41f4c7cb8ba257c1afe2eaa
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 20:27:28 -0500

    adding getters/setters

--- community_app/src/main/java/com/example/community_app/models/Review.java
+++ community_app/src/main/java/com/example/community_app/models/Review.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "")
+@Table(name = "reviews")
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +18,31 @@ public class Review {
     @Column
     private String message;
 
+    //constructor
+    public Review() {}
+
+    public void setMessageid(int messageid) {
+        this.messageid = messageid;
+    }
+
+    public int getMessageid() {
+        return messageid;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 
 }
