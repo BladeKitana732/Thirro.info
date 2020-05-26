3c20a08d815fa6c92d57dc98e21bf6accc7b9c92
Teresa Malave <dev.malave7@gmail.com>
2020-05-26 07:36:04 -0500

    thirdapi call met for project requirement :D

--- thirro-client/src/components/ThirdApi.js
+++ thirro-client/src/components/ThirdApi.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const programmingJokesApi= `https://sv443.net/jokeapi/v2/joke/Programming?format=json`;
+
 export default class ThirdApi extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            joke: "Click button to render random joke!"
+        }
+
+        //get joke
+        this.getJoke = this.getJoke.bind(this);
+    }
+
+     getJoke() {
+        axios.get(programmingJokesApi)
+
+        .then(Response => {
+            
+            this.setState({
+                joke: Response.data.joke
+            })
+            console.log(Response);
+        })
+        
+        
+
+        .catch(error => {
+            console.log("Error message: ", error);
+        })
+
+    }
 
 
+    
 
     render() {
         return (
             <div>
-                <h1>Third api sanity check!</h1>
+                <h1>Dev Funnies</h1>
+
+            <h3>{'"' + this.state.joke + '"'}</h3>
+
+            <button type="button" onClick={this.getJoke}>More Jokes!</button>
             </div>
         )
     }
