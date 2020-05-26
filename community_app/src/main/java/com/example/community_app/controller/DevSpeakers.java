dd4152b1ba34eff98d2e7489ad801b93d2c12778
Teresa Malave <dev.malave7@gmail.com>
2020-05-25 23:30:52 -0500

    got empty array when at endpoint; time to hardcode data to display

--- community_app/src/main/java/com/example/community_app/controller/DevSpeakers.java
+++ community_app/src/main/java/com/example/community_app/controller/DevSpeakers.java
@@ -1,18 +1,26 @@
 package com.example.community_app.controller;
 
 import com.example.community_app.models.DevSpeaker;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.community_app.services.SpeakerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dev_speakers")
 public class DevSpeakers {
+    @Autowired
+    SpeakerService speakerService;
+
     @GetMapping
     public Iterable<DevSpeaker> getSpeakerInfo(){
         return speakerService.getSpeakerInfo();
     }
+
+    @PostMapping
+    public DevSpeaker createSpeaker(@RequestBody DevSpeaker devSpeaker){
+        return speakerService.createSpeaker(devSpeaker);
+    }
+
 }
 
 
--- community_app/src/main/java/com/example/community_app/services/SpeakersImplemented.java
+++ community_app/src/main/java/com/example/community_app/services/SpeakersImplemented.java
@@ -3,7 +3,9 @@ package com.example.community_app.services;
 import com.example.community_app.models.DevSpeaker;
 import com.example.community_app.repository.SpeakerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SpeakersImplemented implements SpeakerService{
     @Autowired
     private SpeakerRepo speakerRepo;
