9f5522b26d6dedf17d33cdb038b8b8952e1a9cba
Teresa Malave <dev.malave7@gmail.com>
2020-05-26 05:36:46 -0500

    trying to have it match wireframe

--- thirro-client/src/components/Logo.js
+++ thirro-client/src/components/Logo.js
@@ -4,7 +4,9 @@ import webLogo from './image/Logo.png';
 export default function Logo() {
     return (
         <div>
+           <h1 style={{fontFamily: "monospace"}}>Welcome to
             <img src= {webLogo} alt= "thirroLogo"></img>
+            </h1>
         </div>
     )
 }
